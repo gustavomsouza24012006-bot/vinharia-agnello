cd08924b6e3722d4847832f451c6f395a95e170f
Gustavo Moreira Souza <gustavo.m.souza24012006@gmail.com>
2026-09-25 13:29:57 -0300

    Adição de um sistema de  gerenciamento de vinhos com JavaScript e leves ajustes no CSS

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+let nomeDoVinho = prompt("Digite o nome do vinho:");
+let tipoDoVinho = prompt("Digite o tipo do vinho (Tinto, Branco ou Rosé):");
+let safra = prompt("Digite a safra do vinho:");
+let quantidade = prompt("Digite a quantidade em estoque:");
+
+alert("Cadastro realizado! Veja os detalhes no console.");
+
+alert("A seguir, veja os detalhes do vinho no console.");
+
+console.log("=== CADASTRO DO VINHO ====");
+console.log("Nome do vinho: " + nomeDoVinho);
+console.log("Tipo: " + tipoDoVinho);
+console.log("Safra: " + safra);
+console.log("Quantidade em estoque: " + quantidade);
